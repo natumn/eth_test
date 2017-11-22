7570636b1ce0f848cc1b74f384ca0e6b3015f884
phustly <natsume.pcuse75@gmail.com>
2017-11-22 15:16:35 +0900

    [Add] web3プロパティを取得する各種メソッドを定義

--- eth_test_app/ether-wallet/client/main.js
+++ eth_test_app/ether-wallet/client/main.js
@@ -1,1 +1,18 @@
+Template.nodeStatusComponent.helpers({
+	
+	//接続先ノードを取得
+	currentProvider: function() {
+		return web3.currentProvider.host;
+	},
 
+	//接続先のノードのマイニング状態を確認
+	//マイニング中ならtrue, そうでなければfalse
+	isMining: function() {
+		return web3.eth.mining;
+	},
+
+	//接続先ノードのマイニングのハッシュレートを取得
+	crrentHashrate: function() {
+		return web3.net.peerCount;
+	},
+});
